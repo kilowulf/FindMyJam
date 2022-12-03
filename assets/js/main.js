d17abcbbdd25100fe4fcc7198da7c774b3f63b0b
Aaron Brown <kilonow1275@hotmail.com>
2022-12-02 21:50:40 -0700

    wfwf

--- assets/js/main.js
+++ assets/js/main.js
@@ -308,20 +308,16 @@
   var needsValidation = document.querySelectorAll(".needs-validation");
 
   Array.prototype.slice.call(needsValidation).forEach(function(form) {
-    form.addEventListener(
-      "submit",
-      function(event) {
-        if (!form.checkValidity()) {
-          window.location = "https://findmyjam.netlify.app/index.html";
-          // event.preventDefault();
-          // event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
+    form.addEventListener("submit", function(event) {
+      if (!form.checkValidity()) {
         window.location = "https://findmyjam.netlify.app/index.html";
-      },
-      false
-    );
+        // event.preventDefault();
+        // event.stopPropagation();
+      }
+
+      form.classList.add("was-validated");
+      window.location = "https://findmyjam.netlify.app/index.html";
+    });
   });
 
   /**
