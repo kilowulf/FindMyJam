59da0fd0d79775574aa2297081b67bc387be2924
Aaron Brown <kilonow1275@hotmail.com>
2022-12-02 22:20:13 -0700

    sdegeg

--- assets/js/main.js
+++ assets/js/main.js
@@ -313,7 +313,7 @@
       function(event) {
         if (form.checkValidity()) {
           form.classList.add("was-validated");
-          event.preventDefault();
+          //event.preventDefault();
         } else {
           false;
         }
